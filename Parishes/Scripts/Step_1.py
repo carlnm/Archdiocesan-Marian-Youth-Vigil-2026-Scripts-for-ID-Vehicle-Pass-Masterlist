e8855a93126f1c4fa0844bae80136605de238a3e
Carl Michael Alano <carlmichaelm.alano@Michaels-MacBook-Air.local>
2026-09-17 21:44:43 +0800

    update

--- Parishes/Scripts/Step_1.py
+++ Parishes/Scripts/Step_1.py
@@ -5,7 +5,7 @@ import re
 # SETTINGS
 # ============================================================
 
-INPUT_FILE = "Parishes/Input/Delagates_Names.xlsx" 
+INPUT_FILE = "Parishes/Input/RawData.xlsx" 
 OUTPUT_FILE = "Parishes/Processed_Data_Cleaned.xlsx"
 
 
